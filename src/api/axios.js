12f78141085455ac6757790f7c5cfae60c7c102f
Giridaran <jdgiridaranlandt@gmail.com>
2026-03-06 00:29:54 +0530

    update backend API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://onlineexamsystem-w13l.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
