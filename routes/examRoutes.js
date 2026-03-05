5271cce46c8111f2920c86bd19e7e831fae64c9f
Giridaran <jdgiridaranlandt@gmail.com>
2026-03-06 00:52:13 +0530

    fix api url for production

--- routes/examRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const { createExam, getExams } = require("../controllers/examController");
-const auth = require("../middleware/authMiddleware");
-
-router.post("/create", auth, createExam);
-router.get("/", auth, getExams);
-
-module.exports = router;
